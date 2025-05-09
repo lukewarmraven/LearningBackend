4fbb1fd3e0b47e1edb58ac397d9210a658f83b12
bacquegou <quintoravenluke@gmail.com>
2025-05-09 19:15:03 +0800

    initial chapter 3 commit

---
+++ chapter3/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import path,{ dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+const PORT = process.env.PORT || 1970
+
+// gets the file path from the URL of current module 
+const __filename = fileURLToPath(import.meta.url)
+// get the directory name of the file path
+const __dirname = dirname(__filename)
+// tells express to serve all files from the public folder as static asset
+// and to know where to resolve css requests - in the public folder
+app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.json())
+
+// serving up the html file from the public dir
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server has started at: ${PORT}`)
+})

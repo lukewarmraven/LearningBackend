a237c7e51c7e5bfa3b75d82b6ba4e8714c1b77f5
bacquegou <quintoravenluke@gmail.com>
2025-05-10 18:49:08 +0800

    partial authRoutes

--- chapter3/src/db.js
+++ chapter3/src/db.js
@@ -1,4 +1,22 @@
 import {DatabaseSync} from 'node:sqlite'
 const db = new DatabaseSync(':memory:')
 
-db.exec(``)
+db.exec(`
+    CREATE TABLE user (
+    id INTEGER,
+    username TEXT UNIQUE AUTOINCREMENT,
+    password TEXT
+    )
+`)
+
+db.exec(`
+    CREATE TABLE todos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN, 
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+`)
+
+export default db
--- chapter3/src/server.js
+++ chapter3/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path,{ dirname } from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 1970
@@ -19,7 +20,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
-
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server has started at: ${PORT}`)
